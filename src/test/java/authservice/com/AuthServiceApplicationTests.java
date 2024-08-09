70575ca8cef10bdeb81ca8f6f57e502e6d90c009
varun724842 <chauhanvarun726@gmail.com>
2024-08-09 21:18:14 +0530

    first commit

---
+++ src/test/java/authservice/com/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package authservice.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
